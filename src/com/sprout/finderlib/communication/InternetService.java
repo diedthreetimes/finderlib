63eef332e670e5631a44e68374d78393a4527fa6
Sky Faber <sky.faber@gmail.com>
2015-02-27 13:29:33 -0800

    socket based com service

--- src/com/sprout/finderlib/communication/InternetService.java
+++ src/com/sprout/finderlib/communication/InternetService.java
@@ -1,66 +1,126 @@
 package com.sprout.finderlib.communication;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Set;
 
 public class InternetService extends AbstractCommunicationService {
-
-  @Override
-  public boolean isEnabled() {
-    // TODO Auto-generated method stub
-    return false;
+  
+  int port;
+  
+  SocketChannel connected;
+  DataInputStream mmInStream;
+  DataOutputStream mmOutStream;
+  
+  public InternetService(int port) {
+    super();
+    
+    this.port = port;
   }
 
   @Override
   public void start(boolean secure) {
-    // TODO Auto-generated method stub
-
+    ServerSocketChannel ssc;
+    try {
+      ssc = ServerSocketChannel.open();
+      ssc.socket().bind(new InetSocketAddress(port) );
+      
+      connected = ssc.accept();
+      
+      setupStreams();
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 
   @Override
   public void connect(String address, boolean secure) {
-    // TODO Auto-generated method stub
-
+    try {
+      connected = SocketChannel.open();
+      connected.connect(new InetSocketAddress(address, port));
+     
+      setupStreams();
+    
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  private void setupStreams() {
+    try {
+      mmInStream = new DataInputStream( connected.socket().getInputStream());
+      mmOutStream = new DataOutputStream( connected.socket().getOutputStream());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public void stop() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void pause() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void resume() {
-    // TODO Auto-generated method stub
-
+    try {
+      connected.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public void write(byte[] out) {
-    // TODO Auto-generated method stub
-
+    try {
+      mmOutStream.writeInt(out.length);
+      mmOutStream.write(out);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
+ 
   @Override
   public byte[] read() {
+    try {
+      int bytes = mmInStream.readInt();
+      // This could be done faster using a byte buffer
+      byte[] buffer = new byte[bytes];
+      
+      mmInStream.readFully(buffer, 0, bytes);
+      
+      return buffer;   
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    return null;
+  }
+  
+  // Unsupported methods
+  @Override
+  protected void retry() {
+    
+    // Random backoff and then attempt connect again.
+    // For now we just singalFailed
+    
+    signalFailed();
+  }
+  
+  @Override
+  public Device getSelf() {
     // TODO Auto-generated method stub
     return null;
   }
-
+  
   @Override
   public void setReadLoop(boolean flag) {
-    // TODO Auto-generated method stub
-
+    // No readloop
   }
 
   @Override
   public boolean getReadLoop() {
-    // TODO Auto-generated method stub
     return false;
   }
 
@@ -81,17 +141,21 @@ public class InternetService extends AbstractCommunicationService {
     // TODO Auto-generated method stub
 
   }
-
+  
   @Override
-  public Device getSelf() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  protected void retry() {
+  public void pause() {
     // TODO Auto-generated method stub
 
   }
 
+  @Override
+  public void resume() {
+    // TODO Auto-generated method stub
+
+  }
+  
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
